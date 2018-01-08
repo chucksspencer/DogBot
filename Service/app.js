8892ac4764477e710cc9090e364d0d65dbb2317f
Chuck Spencer <chucksspencer@gmail.com>
2018-01-08 03:08:51 +0000

    Moved server code into Client project, added mjpeg server

--- Service/app.js
+++ Service/app.js
@@ -1,3 +1,33 @@
+
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, './Client/src/index.html'))
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.listen(8080, function () {
+  console.log('Example listening on port 8080!');
+});
+
+module.exports = app;
+
+/*
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -11,8 +41,8 @@ var dispenser = require('./routes/dispenser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,9 +50,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/Client/src'));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/dispenser', dispenser);
 
 // catch 404 and forward to error handler
@@ -43,4 +73,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('*', function(req, res) {
+  res.sendfile('./Client/src/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 module.exports = app;
+
+*/
