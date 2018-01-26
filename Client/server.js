55608cfda616893f28a6338e6dfbd80dd7114bc0
Chuck Spencer <chuck@buildbunker.com>
2018-01-25 22:28:56 -0600

    Updated server for CORS

--- Client/server.js
+++ Client/server.js
@@ -21,6 +21,12 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 app.use('/dispenser', dispenser);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
